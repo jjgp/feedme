a62d4929a66f82db4ffe2f5fce6730c80aeeff6a
Jason Prasad <jasongprasad@gmail.com>
2022-07-23 14:24:03 +0100

    Adds failure expectation

--- Tests/iOS/API/HTTPTests.swift
+++ Tests/iOS/API/HTTPTests.swift
@@ -18,9 +18,35 @@ class HTTPRequestPublisherTests: XCTestCase {
         let sut = try HTTP(host: "http://reddit.com")
         let getRequest = HTTPRequest<RedditModel.Listing>(method: .get, path: ".json")
 
-        _ = Just(getRequest)
+        let expect = expectation(description: "Listing is received")
+        let expectCompletion = expectation(description: "complete is called")
+        let subscription = Just(getRequest)
             .tryMap {
                 try sut.urlRequest(from: $0)
             }
+            .flatMap {
+                sut
+                    .session
+                    .dataTaskPublisher(for: $0)
+                    .tryMap { element -> Data in
+                        guard let httpResponse = element.response as? HTTPURLResponse,
+                              200 ..< 400 ~= httpResponse.statusCode
+                        else {
+                            throw URLError(.badServerResponse)
+                        }
+                        return element.data
+                    }
+                    .decode(type: RedditModel.Listing.self, decoder: JSONDecoder())
+            }
+            .sink(receiveCompletion: { error in
+                print(error)
+                expectCompletion.fulfill()
+            }, receiveValue: { model in
+                print(model)
+                expect.fulfill()
+            })
+
+        wait(for: [expect, expectCompletion], timeout: .infinity)
+        subscription.cancel()
     }
 }
